e2c920fb789c4354a37f4c527b3038933c2d175e
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2023-08-22 17:14:12 +0800

    Add files via upload

---
+++ Examples/01_Quick guide/HelloWorld.py
@@ -0,0 +1,21 @@
+﻿from spire.pdf.common import *
+from spire.pdf import *
+
+outputFile = "HelloWorld.pdf"
+
+#Create a pdf document
+doc= PdfDocument()
+#Create one page
+page = doc.Pages.Add()
+s = "Hello, World"
+x = 10.0
+y = 10.0
+font = PdfFont(PdfFontFamily.Helvetica ,30.0)
+color = PdfRGBColor(Color.get_Black())
+textBrush = PdfSolidBrush(color)
+#Draw the text
+page.Canvas.DrawString(s, font, textBrush, x, y)
+#Save the document
+doc.SaveToFile(outputFile)
+doc.Close()
+
