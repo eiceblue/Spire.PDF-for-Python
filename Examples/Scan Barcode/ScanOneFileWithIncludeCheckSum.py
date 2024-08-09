50791212b8b167bddd401fec02306ee322bbe2fd
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2024-08-09 15:49:03 +0800

    Delete Examples/Scan Barcode directory

--- Examples/Scan Barcode/ScanOneFileWithIncludeCheckSum.py
+++
@@ -1,8 +0,0 @@
-from spire.barcode import *
-
-def WriteAllText(fpath:str,content:str):
-    with open(fpath,'w',encoding="utf-8") as fp:
-        fp.write(content)
-
-strCode = BarcodeScanner.ScanOneFileWithIncludeCheckSum("QRCode.png",True)
-WriteAllText("ScanOneFileWithIncludeCheckSum.txt",strCode)
